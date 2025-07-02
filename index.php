e07685727c72af4b63120eb3a0db4ed3f0b395b3
DzahwanMalik <malikdzahwan22@gmail.com>
2025-07-02 21:47:32 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium consequuntur rerum culpa eligendi eum nesciunt a maxime neque natus architecto voluptatibus dolor quis praesentium ea, ab, totam ratione libero nisi.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
